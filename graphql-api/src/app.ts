b005ca35a4a2da9f14346e72e02a820506bff68e
Reyna Tan <reynat@seek.com.au>
2022-09-16 17:34:18 +1000

    Solution for part 1

--- graphql-api/src/app.ts
+++ graphql-api/src/app.ts
@@ -1,17 +1,23 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { MusicAPI } from "./data-source";
+import { resolvers } from "./resolvers";
 
 export interface Context {
   dataSources: DataSources;
 }
 
-interface DataSources {}
+interface DataSources {
+  musicAPI: MusicAPI;
+}
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers: [],
-  dataSources: () => ({}),
+  resolvers,
+  dataSources: () => ({
+    musicAPI: new MusicAPI(),
+  }),
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
