f8df688f61e911641034f13e1b0c7528a65e723a
Reyna Tan <reynat@seek.com.au>
2022-09-16 23:32:41 +1000

    Parent object passing to get an artist for an album

--- graphql-api/src/data-source.ts
+++ graphql-api/src/data-source.ts
@@ -1,6 +1,18 @@
 import { RESTDataSource } from "apollo-datasource-rest";
 import { ApolloError } from "apollo-server-core";
 
+export type ArtistResponse = {
+    id: string;
+    name: string;
+    country: string;
+}
+
+export type AlbumResponse = {
+    id: string;
+    title: string;
+    artist: string;
+}
+
 export class MusicAPI extends RESTDataSource {
   constructor() {
     super();
@@ -11,12 +23,24 @@ export class MusicAPI extends RESTDataSource {
     return this.get("/albums");
   }
 
-  async getAlbum(id: string) {
+  async getAlbum(id: string): Promise<AlbumResponse | undefined> {
     return this.get(`/albums/${id}`)
       .then((albumResponse) => albumResponse)
       .catch((error: ApolloError) => {
         if (error.extensions.response?.status === 404) {
           console.log(`Album id ${id} not found`);
+          return;
+        }
+        return error;
+      });
+  }
+
+  async getArtist(id: string): Promise<ArtistResponse | undefined> {
+    return this.get(`/artists/${id}`)
+      .then((albumResponse) => albumResponse)
+      .catch((error: ApolloError) => {
+        if (error.extensions.response?.status === 404) {
+          console.log(`Artist id ${id} not found`);
           return;
         }
         return error;
