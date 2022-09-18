63552b78d716a2094ccf8efc21de469e33143c89
Reyna Tan <reynat@seek.com.au>
2022-09-18 22:15:51 +1000

    Test for album by id

--- graphql-api/src/resolvers.test.ts
+++ graphql-api/src/resolvers.test.ts
@@ -7,11 +7,18 @@ import { typeDefs } from "./schema";
 describe("album Query resolver", () => {
   it("should return an album when a valid ID is provided", async () => {
     const query = gql`
-        """insert graphql query"""
+      query {
+        album(id: "valid-album-id") {
+          title
+        }
+      }
     `;
     const musicAPI = mock<MusicAPI>();
-    // Insert test response data
-    const testAlbumResponse: AlbumResponse = {};
+    const testAlbumResponse: AlbumResponse = {
+      id: "valid-album-id",
+      title: "some-title",
+      artist: "valid-artist-id",
+    };
     musicAPI.getAlbum.mockResolvedValueOnce(testAlbumResponse);
 
     const testServer = new ApolloServer({
@@ -26,7 +33,40 @@ describe("album Query resolver", () => {
       query,
     });
 
-    expect(result.data).toEqual({});
-    expect(result.errors).toEqual({});
+    expect(result.data).toEqual({
+      album: {
+        title: "some-title",
+      },
+    });
+    expect(result.errors).toBeUndefined();
+  });
+
+  it("should return null with no errors when an invalid ID is provided and musicAPI returns undefined", async () => {
+    const query = gql`
+      query {
+        album(id: "invalid-album-id") {
+          title
+        }
+      }
+    `;
+    const musicAPI = mock<MusicAPI>();
+    musicAPI.getAlbum.mockResolvedValueOnce(undefined);
+
+    const testServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      dataSources: () => ({
+        musicAPI,
+      }),
+    });
+
+    const result = await testServer.executeOperation({
+      query,
+    });
+
+    expect(result.data).toEqual({
+      album: null,
+    });
+    expect(result.errors).toBeUndefined();
   });
 });
